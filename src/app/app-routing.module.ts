3e8d563fe71ccaf373541f97c963330e209265cf
Yusuf Yalim <yusufyalim@gmail.com>
2020-06-27 13:12:45 +0300

    Add navigation links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ import { LastMonthComponent } from './last-month/last-month.component';
 import { TopFiveComponent } from './top-five/top-five.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/currency/latest' },
   { path: 'currency/latest', component: LatestComponent },
   { path: 'currency/last-month', component: LastMonthComponent },
-  { path: 'currency/top-five', component: TopFiveComponent }
+  { path: 'currency/top-five', component: TopFiveComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '/currency/latest' }
 ];
 
 @NgModule({
