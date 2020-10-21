31280bbc5f88a2d7c442969b08011781e1ad69f2
Yusuf Yalim <yusuf.yalim@staxter.com>
2020-10-21 11:37:09 +0200

    Add prerendering service

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+const PRERENDER_TOKEN = process.env.PRERENDER_TOKEN || '';
+
 //Install express server
 const express = require("express");
 const path = require("path");
+const prerender = require("prerender-node");
+
+prerender.crawlerUserAgents.push('googlebot');
+prerender.crawlerUserAgents.push('bingbot');
+prerender.crawlerUserAgents.push('yandex');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/staxter-currency"));
+
+app.use(prerender.set('prerenderToken', PRERENDER_TOKEN));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/staxter-currency/index.html"));
